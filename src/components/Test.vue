d1cc8dce4a6231bcb212f92dc79171aa35f21494
Skid Vis <skid@skidvis.com>
2023-03-08 19:45:05 -0600

    error when importing component into component

---
+++ src/components/Test.vue
@@ -0,0 +1,18 @@
+<script>
+    import TradingVue from 'trading-vue-js';
+
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        components: {
+            TradingVue
+        }
+    }
+</script>
+
+<template>
+    <trading-vue></trading-vue>
+</template>
